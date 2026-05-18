d41363124b8b2ae811af6c96a51d0a72ff0af987
Suruthi BT <suruthibabu30@gmail.com>
2026-05-18 22:36:08 +0530

    Changes1
    Changes1

---
+++ script.js
@@ -0,0 +1,25 @@
+
+   console.log("Welcome to my journey!"); 
+/*
+ <script>
+  function navigate(event, link) {
+    event.preventDefault(); // stop instant jump
+
+    const overlay = document.querySelector(".transition");
+    overlay.classList.add("active");
+
+    setTimeout(() => {
+      window.location.href = link.href;
+    }, 600); // match CSS timing
+  }
+
+  // Page load animation (reverse effect)
+  window.onload = () => {
+    const overlay = document.querySelector(".transition");
+    overlay.style.transform = "translateY(0)";
+    
+    setTimeout(() => {
+      overlay.style.transform = "translateY(-100%)";
+    }, 100);
+  };
+</script> */
